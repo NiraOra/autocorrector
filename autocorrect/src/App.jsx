c877cb20c23a30bb383e524b703ab68035a23718
n.arun_menon@student.unsw.edu.au <z5417727@ad.unsw.edu.au>
2025-03-04 15:34:35 +1100

    added autocorrect btn

--- autocorrect/src/App.jsx
+++ autocorrect/src/App.jsx
@@ -4,35 +4,46 @@ import { ReactTyped } from "react-typed";
 import Brightness7RoundedIcon from '@mui/icons-material/Brightness7Rounded';
 import BedtimeRoundedIcon from '@mui/icons-material/BedtimeRounded';
 
-
-
 const App = () => {
   const [lightMode, setLightMode] = useState(false);
-  const [lightModeText, setLightModeText] = useState(false);
-  const [lightModeBtn, setLightModeBtn] = useState(false);
-  const [lightModeHover, setLightModeHover] = useState(false);
-  const [typedString, setTypedString] = useState(["> Autocorrector.."]);
+  const [typedString, setTypedString] = useState(["> Autocorrector"]);
+  const [showTyped, setShowTyped] = useState(true);
 
   const toggleLightMode = () => {
     setLightMode((prevMode) => !prevMode);
-    setLightModeText((prevMode) => !prevMode);
-    setLightModeBtn((prevMode) => !prevMode);
-    setLightModeHover((prevMode) => !prevMode);
   };
 
   return (
     <div className={lightMode ? "App-header-light" : "App-header-dark"}>
-      <p className={lightModeText ? "font-main-light" : "font-main-dark"}> 
-        <ReactTyped
-          strings={typedString}
-          typeSpeed={100}
-          cursorChar="."
-          showCursor={true}
-          className={lightModeHover ? "typed-text-light" : "typed-text-dark"}
-        />
+      {/* <header className={lightModeText ? "font-main-light" : "font-main-dark"}>
+        Click on the "Autocorrect" to begin input text
+      </header> */}
+      <p className={lightMode ? "font-main-light" : "font-main-dark"}> 
+        {showTyped ? 
+          <span onClick={() => setShowTyped(false)}>
+            <ReactTyped
+              strings={typedString}
+              typeSpeed={100}
+              cursorChar="."
+              showCursor={true}
+              className={lightMode ? "font-main-light" : "font-main-dark"}
+            />
+          </span>
+          : <>
+              <input
+                  type="text"
+                  value={typedString}
+                  onChange={(e) => setTypedString(e.target.value)}
+                  className={lightMode ? "input-light" : "input-dark"}
+                  autoFocus
+                />
+                <br />
+              <button className="autocorrect-btn"> Auto-Correct </button>
+            </>
+          }
       </p>
       <div>
-        <button onClick={toggleLightMode} className={lightModeBtn ? "toggle-btn" : "toggle-btn"}>
+        <button onClick={toggleLightMode} className={lightMode ? "toggle-btn" : "toggle-btn"}>
           {lightMode ? <Brightness7RoundedIcon /> : <BedtimeRoundedIcon />}
         </button>
       </div>
